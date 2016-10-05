70167d2435a4530b8b7c73e07d71e7d125188539
mdwasim3 <md.wasim3@gmail.com>
2016-10-05 20:08:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/artical-one',function(req,res){
    res.sendFile(path.join(__dirname, 'ui', 'artical-one.html'));
 });
 app.get('/artical-two',function(req,res){
-   res.send('THIs is artical TWO'); 
+  res.sendFile(path.join(__dirname, 'ui', 'artical-two.html')); 
 });
 app.get('/artical-three',function(req,res){
    res.send('THIs is artical THREE'); 
