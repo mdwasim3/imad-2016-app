432807e62a2dc8f52306415040c89002c9ca80eb
mdwasim3 <md.wasim3@gmail.com>
2016-10-06 13:18:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,7 +4,40 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+ var articalOne= {
+        title: 'Artical One',
+        date: '10 oct 16',
+        content: `
+        <table border="1">
+  <tr>
+  <td> hello col 1 and row one </td>
+  <td> hello col 1 row two </td>
+  </tr>
+  
+</table>
+<h3> hello buddy</h3>
+<pre>
+    
+    hello    kuch to bolo.
+</pre>
+<h2>hi this is the color going to change</h2>
+<marquee> hi this is moving </marquee>
+<pre>Hello, it's me
+I was wondering if after all these years you'd like to meet
+To go over everything
+They say that time's supposed to heal ya
+But I ain't done much healing </pre>
+<code>
+Hello, can you hear me?
+I'm in California dreaming about who we used to be
+When we were younger and free
+I've forgotten how it felt before the world fell at our feet</code>
 
+There's such a difference between us
+And a million miles `
+     
+    
+ };
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
