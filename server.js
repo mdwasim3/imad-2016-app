c7f2c4ccf5114a89c9614de0d317be156dc95912
mdwasim3 <md.wasim3@gmail.com>
2016-10-07 12:52:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,30 +1,88 @@
-<html>
-	<head>
-		<title>
-		  Artical two 
-		</title>
-		<meta name="viewpoint" content="width=device-width,initial-scale=1" />
-		<link href="/ui/style.css" rel="stylesheet" />
-	</head>
-	<body>
-		<div class="container">
-			<div>
-				<a href="/"> Home </a>
-			</div>
-			<hr/>
-			<h3>
-				Artical two
-			</h3>
-			<div>
-				Sep 25, 2018
-			</div>
-			<div>
-				<p> This is my two Artical.  
-					This is my two Artical.  
-					This is my two Artical.  
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
+
+var app = express();
+app.use(morgan('combined'));
+
+
+
+var articalOne= {
+        title: 'Artical One',
+        heading:'Artical  One',
+        date: '10 oct 16',
+        content: `
+        	<p> This is my first Artical.  THIs is my first artical
+					This is my first Artical.  THIs is my first artical
+					This is my first Artical.  THIs is my first artical
 				</p>
-				
-			</div>	
-		</div>
-	</body>
-</html>
+				<p>
+				This is my first Artical.  THIs is my first articalThis is my first Artical.  THIs is my first articalThis is my first Artical.  THIs is my first artical
+				</p>
+				<p>
+				This is my first Artical.  THIs is my first articalThis is my first Artical.  THIs is my first articalThis is my first Artical.  THIs is my first artical..
+				</p>`
+     
+    
+ };
+ 
+ 
+ function createTemplate(data) {
+ var title=data.title;
+ var date=data.date;
+ var heading=data.heading;
+ var content=data.content;
+
+var htmlTemplate= `
+	<html>
+ 	<head>  <title>  
+ 			${title}
+ 			</title>
+ 			<meta name="viewpoint" content="width=device-width,initial-scale=1" />
+ 			<link href="/ui/style.css" rel="stylesheet" />
+ 			</div>
+ 			<hr/>
+ 			<h3>
+ 							${heading}
+ 			</h3>
+ 			 			<div>
+ 			 			${date}
+ 			 			</div>
+ 			 			<div>
+ 			 			${content}
+ 			 			</div>	
+ 			 			</div>
+ 			 		</body>
+ 			 		 </html>
+ 			 		  `;
+ 			 		  return htmlTemplate;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+
+var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+app.listen(8080, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+});
