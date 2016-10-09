7d4fa780b1d2bc574739fc9a19b6ff132e82bf1d
mdwasim3 <md.wasim3@gmail.com>
2016-10-09 16:40:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,3 @@ console.log('Loaded!');
 var e=document.getElementById("hello");
 e.innerHTML="CHANGE HAPPENED";
 
-myFunction()
-{
-    document.getElementById("wasim").style.marginLeft = "300px" ;
-}
