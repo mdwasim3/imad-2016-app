2a2cd89e1f4ecc7ba9e8eb5a8de32270d4f2beb1
mdwasim3 <md.wasim3@gmail.com>
2016-10-09 16:27:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,9 +6,3 @@ console.log('Loaded!');
 var e=document.getElementById("hello");
 e.innerHTML="CHANGE HAPPENED";
 
-//image moving function
-
-var img = document.getElementById('wasim');
-img.onClick = myfunction(){
-    img.style.marginLeft= '500px' ;
-}
