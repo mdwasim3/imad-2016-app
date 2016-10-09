349611ad3e690cb555821efc78992b9ca7c650c1
mdwasim3 <md.wasim3@gmail.com>
2016-10-09 08:54:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,7 @@ console.log('Loaded!');
 
 var element=document.getElementById('test');
 element.innerHtml= " tis is the main serverd Inner Html";
+
+
+var helloele=document.getElementById('hello');
+helloele.innerHtml= " tis is the main serverd Inner Html";
