00359455c32b31fb2772935edb1c6e412ad0da1d
mdwasim3 <md.wasim3@gmail.com>
2016-10-10 18:25:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,6 +81,13 @@ var htmlTemplate= `
 }
 
 
+//counter example
+var counter = 0;
+app.get('/counter',function(req,res){
+    counter = counter + 1;
+    res.send(counter.toString());
+});
+
 
 
 app.get('/', function (req, res) {
