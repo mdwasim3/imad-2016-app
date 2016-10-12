057380b224cf41120bf51d8ba6ac706375a9cbbf
mdwasim3 <md.wasim3@gmail.com>
2016-10-12 19:01:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,29 @@
-console.log('Loaded!');
 
 
-
-
-     
-     
-// variable counter 
-    
- 
-var counter = 0;
 var button = document.getElementById('counter');
-button.onclick = function(){
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+
+button.onclick = function() {
+	// create the request
+
+          var request = new XMLHttpRequest();
+
+	// Capture the response and store it in a variable
+
+	    request.onreadystatechange = function(){
+		if (request.readyState === XMLHttpRequest.DONE){
+	//Take some action
+		if(request.status === 200){
+			var counter = request.responseText;
+			var span = document.getElementById('count');
+ 			span.innerHTML = counter.toString();
+
+		}
+	     }
+	// Not done yet
+	};
+	// Make the request
+		request.open('GET', 'http://localhost:8080/counter', true);
+		request.send(null); 
+
+
 };
