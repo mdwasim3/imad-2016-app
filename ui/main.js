0c6f4ee52962f8b4bb6dc09b27141cc1ab128737
mdwasim3 <md.wasim3@gmail.com>
2016-10-12 19:25:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,8 +14,8 @@ button.onclick = function() {
 	//Take some action
 		if(request.status === 200){
 			var counter = request.responseText;
-			var span = document.getElementById('count');
- 			span.innerHTML = counter.toString();
+			var span1 = document.getElementById('count1');
+ 			span1.innerHTML = counter.toString();
 
 		}
 	     }
