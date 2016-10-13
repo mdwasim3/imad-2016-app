14471e73de2b6e237fddf4f668b00a45896ea3c5
mdwasim3 <md.wasim3@gmail.com>
2016-10-13 12:19:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -119,6 +119,20 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 
+var names[];
+app.get('/submit-name/:name', function(req,res) {
+  
+//Get the name from the request
+var name = req.params.name;
+names.push(name);
+res.send(JSON.stringify(names));
+
+});
+
+
+
+
+
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
