7eefac4a1c0935772e29fac7757ccae50f43a3ff
mdwasim3 <md.wasim3@gmail.com>
2016-10-13 18:15:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,6 +90,14 @@ app.get('/counter',function(req,res){
 
 
 
+var names[];
+app.get('/submit-name/:name', function(req,res) {
+var name = req.params.name;
+names.push(name);
+res.send(JSON.stringify(names));
+
+});
+
 
 
 
@@ -120,16 +128,6 @@ app.get('/ui/style.css', function (req, res) {
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-var names[];
-app.get('/submit-name/:name', function(req,res) {
-  
-
-var name = req.params.name;
-names.push(name);
-res.send(JSON.stringify(names));
-
 });
 
 
